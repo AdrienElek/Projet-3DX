e17b4960c12e6bdf037751935118b0ba2f651750
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-05-12 14:29:02 +0200

    feat(sound): add sound and play music on start

---
+++ Assets/scripts/SoundDesign/BackgroundMusic.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Timeline;
+
+//AUTHOR : Alexandre GAUTIER
+
+public class BackgroundMusic : MonoBehaviour
+{
+    
+    public AudioClip[] backgroundMusicList;
+    public AudioSource source;
+    private int PreviousMusicInt = -1;
+    private float newClip;
+    private float timer;
+
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.volume = 0.2f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > newClip + 1)
+        {
+            PlayBackgroundMusic();
+            timer = 0;
+        }
+    }
+
+    public void PlayBackgroundMusic()
+    {
+        int rand = Random.Range(0, backgroundMusicList.Length);
+        
+        if (PreviousMusicInt == rand)
+        {
+            while (PreviousMusicInt == rand)
+            {
+                rand = Random.Range(0, backgroundMusicList.Length);
+            }
+        }
+        PreviousMusicInt = rand;
+
+        if (!source.isPlaying)
+        {
+            source.loop = true;
+            source.PlayOneShot(backgroundMusicList[rand]);
+        }
+
+        newClip = backgroundMusicList[rand].length;
+    }
+}
