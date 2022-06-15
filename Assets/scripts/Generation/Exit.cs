2fe5d16b2ff29adeed59c13aae97138581de35df
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-06-15 13:56:10 +0200

    conflict exit.cs

--- Assets/scripts/Generation/Exit.cs
+++ Assets/scripts/Generation/Exit.cs
@@ -8,11 +8,6 @@ public class Exit : MonoBehaviour
         void Start(){}
     private void OnControllerColliderHit(ControllerColliderHit coll)
     {
-<<<<<<< HEAD
-        // Debug.Log(coll.transform.tag);
-=======
-        //Debug.Log(coll.transform.tag);
->>>>>>> 32b20f35f8e321ae7139a8d40b7ffa5ae1d7b289
         if (coll.transform.tag == "Exit")
         {
             Destroy(TerrainGeneration.boardHolderBlocks.gameObject);
